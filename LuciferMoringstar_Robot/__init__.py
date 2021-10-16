b243e317cb0b0b01bae85e15ed3e22373c840554
Anderson <91043752+Anderson-Johny-George17@users.noreply.github.com>
2021-10-16 12:56:16 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -33,7 +33,7 @@ HELP = """
 """
 
 ABOUT = """
-➥ Name : [Auto Filter With IMDB](t.me/Flixautofilter1_bot
+➥ Name : [Auto Filter With IMDB](t.me/Flixautofilter1_bot) 
     
 ➥ Dev  : [Anderson](t.me/Anderson_Johny) 
 
