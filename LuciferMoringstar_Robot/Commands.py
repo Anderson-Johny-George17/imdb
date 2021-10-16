836ba8aee31fa3a92058a741d36f9ff6474f84c9
Anderson <91043752+Anderson-Johny-George17@users.noreply.github.com>
2021-10-16 22:30:12 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -372,7 +372,8 @@ async def bot_info(bot, message):
         ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
          
-         @Client.on_message(filters.command(["reqformat"]))
+
+@Client.on_message(filters.command(["reqformat"]))
 
 async def reqformat(bot, message):
 
