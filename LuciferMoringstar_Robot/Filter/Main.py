3b8c726c53fc745f1649eeef9229701e99173f44
Anderson <91043752+Anderson-Johny-George17@users.noreply.github.com>
2021-10-16 11:39:54 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -57,16 +57,16 @@ async def filter(client, message):
         files = await get_filter_results(query=search)
         if files:
             for file in files:
+
                 file_id = file.file_id
-                file_name = file.file_name
-                file_size = get_size(file.file_size)
-                file_link = f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}"
+
+                filename = f"[{get_size(file.file_size)}] {file.file_name}"
+
                 btn.append(
-                    [
-                      InlineKeyboardButton(text=f"🎬 {file_name}", url=f"{file_link} 📂"),
-                      InlineKeyboardButton(text=f"🎬 {file_size}", url=f"{file_link} 📂")
-                    ]
-                )
+
+                    [InlineKeyboardButton(text=f"{filename}",callback_data=f"pr0fess0r_99#{file_id}")]
+
+                    )
         else:
             await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
             return
